1b2e26435f95442d44b6bf203d15790c3d165a12
Thanh Nguyen <hthanh1192@gmail.com>
2020-09-14 18:43:17 +0700

    create header + add logo

---
+++ themes/gymfitnesswptheme/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<header class="site-header">
+    <div class="container header-grid">
+        <div class="navigation-bar">
+            <div class="logo">
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_template_directory_uri() . "/img/logo.svg" ?>" alt="Site logon">
+                </a>
+            </div><!--/logo-->
+            <!-- Todo add menu -->
+        </div><!--.navigation-bar-->
+    </div><!--.container-->
+</header>
