222aa56ee165746fbda3e4aee0a6f7623475b370
Thanh Nguyen <hthanh1192@gmail.com>
2020-09-14 17:59:40 +0700

    use while get post to single.php and page.php

--- themes/gymfitnesswptheme/page.php
+++ themes/gymfitnesswptheme/page.php
@@ -1,3 +1,4 @@
-<?php
-    echo "Hello from page.php";
-?>
+<?php while(have_posts()): the_post(); ?>
+    <h1><?php the_title() ?></h1>
+    <p><?php the_content() ?></p>
+<?php endwhile;?>
--- themes/gymfitnesswptheme/single.php
+++ themes/gymfitnesswptheme/single.php
@@ -1,3 +1,4 @@
-<?php
-    echo "hello from single.php";
-?>
+<?php while(have_posts() ): the_post(); ?>
+    <h1><?php the_title() ?></h1>
+    <p><?php the_content() ?></p>
+<?php endwhile;?>
