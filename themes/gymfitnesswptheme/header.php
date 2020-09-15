7467b32b7779e96ecf8838f8b25140d4088019ba
Thanh Nguyen <hthanh1192@gmail.com>
2020-09-15 16:12:54 +0700

    reder menu in header

--- themes/gymfitnesswptheme/header.php
+++ themes/gymfitnesswptheme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php wp_head() ?>
 </head>
 <body>
     
@@ -15,6 +15,15 @@
                     <img src="<?php echo get_template_directory_uri() . "/img/logo.svg" ?>" alt="Site logo">
                 </a>
             </div><!--/logo-->
+
+            <?php
+                $args = array(
+                    'them_location' => 'main-menu',
+                    'container' => 'nav',
+                    'container_class' => 'main-menu'
+                );
+                wp_nav_menu( $args);
+            ?>
             <!-- Todo add menu -->
         </div><!--.navigation-bar-->
     </div><!--.container-->
