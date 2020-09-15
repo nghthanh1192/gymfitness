13439ea1f3f808fa0f81d3d53fd43fc2e9bb1e26
Thanh Nguyen <hthanh1192@gmail.com>
2020-09-15 16:44:20 +0700

    add normalize.css style, google fonts, style.css main

--- themes/gymfitnesswptheme/functions.php
+++ themes/gymfitnesswptheme/functions.php
@@ -3,11 +3,23 @@
     function gymfitness_menus() {
         // WordPress function
         register_nav_menus( array(
-            'main-menu' => 'Main Menu',
-            'social-menu' => 'Social Menu',
-            'child-menu' => 'Child Menu'
+            'main-menu' => 'Main Menu'
         ) );
     }
 
     // Hook (init)
     add_action( 'init', 'gymfitness_menus');
+
+    // Adds Stylesheets and JS files
+    function gymfitness_scripts() { 
+        // Normalize CSS
+        wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+        // Google Font
+        wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
+
+        // Main Font
+        wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googlefont'), '1.0.0');
+    }
+
+    add_action('wp_enqueue_scripts','gymfitness_scripts');
