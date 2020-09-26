eb49ee8f0bfe979c46db7b143a4b7432b75a7b53
Thanh Nguyen <hthanh1192@gmail.com>
2020-09-26 12:51:00 +0700

    building a pluggin for widget, add query into widget

---
+++ plugins/gymfitness_widgets/gymfitness_widgets.php
@@ -0,0 +1,98 @@
+<?php
+/*
+    Plugin Name: Gym Fitness - Widgets
+    Plugin URI:
+    Description: Add a Custom Widget in the Wordpress Panel
+    Version: 1.0.0
+    Author: Nguyen Huu Thanh
+    Author URI: fb.com/nguyenhuuthanh11
+    Text Domain: gymfitness
+*/
+
+if (!defined('ABSPATH')) die();
+
+/**
+ * Adds GymFitness_Classes_Widget widget.
+ */
+class GymFitness_Classes_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'gymfitness_classes', // Base ID
+			esc_html__( 'GymFitness - Classes List', 'text_domain' ), // Name
+			array( 'description' => esc_html__( 'Display different classes in the widget', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget']; ?>
+		<ul class="sidebar-classes-list">
+			<?php
+				$args = array (
+					'post_type' => "gymfitness_classes",
+					'posts_per_page' => 2	
+				);
+				$classes = new WP_Query($args);
+				while ($classes->have_posts()): $classes->the_post();
+			?>
+				<li class="sidebar-class">
+					<h3><?php the_title(); ?></h3>
+				</li>
+			<?php endwhile; wp_reset_postdata(); ?>	
+		</ul>
+		<?php
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class GymFitness_Classes_Widget
+
+// register GymFitness_Classes_Widget widget
+function gymfitness_classes_widget() {
+    register_widget( 'GymFitness_Classes_Widget' );
+}
+add_action( 'widgets_init', 'gymfitness_classes_widget' );
