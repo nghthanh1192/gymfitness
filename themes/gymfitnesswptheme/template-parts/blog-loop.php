f9ca3118ee988c3b690a11e3f3c875754920aa17
Thanh Nguyen <hthanh1192@gmail.com>
2020-10-05 11:35:34 +0700

    add category into blog & css

--- themes/gymfitnesswptheme/template-parts/blog-loop.php
+++ themes/gymfitnesswptheme/template-parts/blog-loop.php
@@ -3,6 +3,9 @@
     <?php while(have_posts()): the_post(); ?>
         <li class="card gradient">
             <?php the_post_thumbnail('mediumSize');?>
+
+            <?php the_category(); ?>
+
             <div class="card-content">
                 <a href="<?php the_permalink(); ?>">
                     <h3><?php the_title(); ?></h3>
