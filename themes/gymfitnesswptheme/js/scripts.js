80d481892e60f42088239e2e3b5d686c2030f517
Thanh Nguyen <hthanh1192@gmail.com>
2020-10-15 11:34:35 +0700

    menu fix

--- themes/gymfitnesswptheme/js/scripts.js
+++ themes/gymfitnesswptheme/js/scripts.js
@@ -6,7 +6,32 @@ jQuery(document).ready(function ($) {
 
     // Run the BxSlider library on testimonials
     $('.testimonials-list').bxSlider({
-        control: false,
+        controls: false,
         mode: 'fade' 
     });
+
+    // When the page is ready add the fixed menu if position is greater than 300px
+    const headerScroll = document.querySelector('.navigation-bar');
+    const rect = headerScroll.getBoundingClientRect();
+    const topDistance = Math.abs(rect.top);
+    fixedMenu(topDistance);
+
 });
+
+window.onscroll = () => {
+    const scroll = window.scrollY;
+    
+    fixedMenu(scroll);
+}
+
+// Adds a fixed menu on top
+function fixedMenu(scroll) {
+    const headerScroll = document.querySelector('.navigation-bar');
+
+    // In the case that the scroll is greater than 300 add some classes
+    if(scroll > 300) {
+        headerScroll.classList.add('fixed-top');
+    } else {
+        headerScroll.classList.remove('fixed-top');
+    }
+}
